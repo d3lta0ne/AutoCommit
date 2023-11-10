bc1ea26a6f88d2f60e45be342af0681fe5939f12
Delta One <39011780+d3lta0ne@users.noreply.github.com>
2023-11-10 12:41:03 -0500

    use subprocess instead of os

--- autocommit.py
+++ autocommit.py
@@ -2,31 +2,32 @@
 from dotenv import load_dotenv
 import datetime
 import os
+import subprocess
+
 load_dotenv()
 
 print("****Script started****")
 file_path = os.getenv('FILE_PATH', 'file.txt')
-# Set the file path and name
 
+# Set the file path and name
 
 # Create or update the file with the current date
 current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("****File opened****")
 with open(file_path, "w") as file:
     file.write(current_date)
-    file.close()
     print("File closed")
 
 # Commit the file to the GitHub repository
 commit_message = f"Auto commit: Updated date to {current_date}"
 
 # Github Commit Messages
-os.system('git add "{}"'.format(file_path))
+subprocess.run(['git', 'add', file_path], check=True)
 print("****Git Add****")
 
-os.system('git commit -m "{}"'.format(commit_message))
+subprocess.run(['git', 'commit', '-m', commit_message], check=True)
 print("****Git Commit****")
 
-os.system('git push')
+subprocess.run(['git', 'push'], check=True)
 print("****Git Pushed****")
 print("****Script ended****")
